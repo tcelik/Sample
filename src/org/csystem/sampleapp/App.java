9d7a811fdd83e1f274076674f8b1bd6d9d627505
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-06 22:11:30 +0300

    Generic bir arrayList'i iterable yapmak, arka planda olan

--- src/org/csystem/sampleapp/App.java
+++ src/org/csystem/sampleapp/App.java
@@ -3,6 +3,8 @@ package org.csystem.sampleapp;
 import org.csystem.collection.CsdGenericArrayList;
 import org.csystem.util.ArrayUtil;
 
+import java.util.Iterator;
+
 public class App {
 
     public static void main(String[] args)
@@ -12,7 +14,15 @@ public class App {
         stringCsdGenericArrayList.add("ankara");
         stringCsdGenericArrayList.add("istanbul");
 
-        for(String str : stringCsdGenericArrayList) {
+        //Arka planda olan
+        Iterator<String> it = stringCsdGenericArrayList.iterator();
+        while (it.hasNext()) {
+            String str = it.next();
+            System.out.println(str);
+        }
+
+        //Arka planı boşver bunu for each yapıcak her adımda next'i vericek.
+        for(String str : stringCsdGenericArrayList){
             System.out.println(str);
         }
     }
